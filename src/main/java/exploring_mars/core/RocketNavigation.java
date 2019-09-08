9f4c3d7212886349f5ed2878088786dee7a3989d
Bruno Luz <bruno.luz@gmail.com>
2019-09-08 19:58:20 -0300

    navigation commands

--- src/main/java/exploring_mars/core/RocketNavigation.java
+++ src/main/java/exploring_mars/core/RocketNavigation.java
@@ -5,11 +5,9 @@ import java.util.List;
 
 public class RocketNavigation {
 
-	List<RocketCoordinate> navigationHistory = new ArrayList<>();
-
-	int maximumX;
-
-	int maximumY;
+	List<RocketCoordinate> navigationHistory = new ArrayList<RocketCoordinate>();
+	private final int maximumX;
+	private final int maximumY;
 
 	public RocketNavigation(int maximumX, int maximumY) throws ExploringMarsException {
 
@@ -26,6 +24,89 @@ public class RocketNavigation {
 
 	public RocketCoordinate getCurrentCoordinate() {
 		return navigationHistory.get(navigationHistory.size() - 1);
+	}
+
+	int getMaximumX() {
+		return maximumX;
+	}
+
+	int getMaximumY() {
+		return maximumY;
+	}
+
+	public RocketCoordinate turnLeft() throws ExploringMarsException {
+
+		RocketCoordinate currentCoordinate = this.getCurrentCoordinate();
+		RocketCoordinate nextCoordinate;
+
+		Direction currentDirection = currentCoordinate.getDirection();
+
+		if (currentDirection == Direction.North)
+			nextCoordinate = new RocketCoordinate(currentCoordinate.getX(), currentCoordinate.getY(), Direction.West);
+		else if (currentDirection == Direction.West)
+			nextCoordinate = new RocketCoordinate(currentCoordinate.getX(), currentCoordinate.getY(), Direction.South);
+		else if (currentDirection == Direction.South)
+			nextCoordinate = new RocketCoordinate(currentCoordinate.getX(), currentCoordinate.getY(), Direction.East);
+		else // West
+			nextCoordinate = new RocketCoordinate(currentCoordinate.getX(), currentCoordinate.getY(), Direction.North);
+
+		this.navigationHistory.add(nextCoordinate);
+
+		return nextCoordinate;
+	}
+
+	public RocketCoordinate turnRight() throws ExploringMarsException {
+
+		RocketCoordinate currentCoordinate = this.getCurrentCoordinate();
+		RocketCoordinate nextCoordinate;
+
+		Direction currentDirection = currentCoordinate.getDirection();
+
+		if (currentDirection == Direction.North)
+			nextCoordinate = new RocketCoordinate(currentCoordinate.getX(), currentCoordinate.getY(), Direction.East);
+		else if (currentDirection == Direction.East)
+			nextCoordinate = new RocketCoordinate(currentCoordinate.getX(), currentCoordinate.getY(), Direction.South);
+		else if (currentDirection == Direction.South)
+			nextCoordinate = new RocketCoordinate(currentCoordinate.getX(), currentCoordinate.getY(), Direction.West);
+		else // West
+			nextCoordinate = new RocketCoordinate(currentCoordinate.getX(), currentCoordinate.getY(), Direction.North);
+
+		this.navigationHistory.add(nextCoordinate);
+
+		return nextCoordinate;
+	}
+
+	public RocketCoordinate move() throws ExploringMarsException {
+
+		RocketCoordinate currentCoordinate = this.getCurrentCoordinate();
+
+		Direction currentDirection = currentCoordinate.getDirection();
+		int currentX = currentCoordinate.getX();
+		int currentY = currentCoordinate.getY();
+		int nextX;
+		int nextY;
+
+		if (currentDirection == Direction.North) {
+			nextX = currentX + 1;
+			nextY = currentY;
+		} else if (currentDirection == Direction.East) {
+			nextX = currentX;
+			nextY = currentY + 1;
+		} else if (currentDirection == Direction.South) {
+			nextX = currentX - 1;
+			nextY = currentY;
+		} else { // West
+			nextX = currentX;
+			nextY = currentY - 1;
+		}
+
+		if (nextX < 0 || nextX > maximumX || nextY < 0 || nextY > maximumY)
+			throw new ExploringMarsException("You've reached the boundaries of mars.");
+
+		RocketCoordinate nextCoordinate = new RocketCoordinate(nextX, nextY, currentDirection);
+		this.navigationHistory.add(nextCoordinate);
+
+		return nextCoordinate;
 	}
 
 }
--- src/test/java/exploring_mars/core/RocketNavigationTest.java
+++ src/test/java/exploring_mars/core/RocketNavigationTest.java
@@ -2,31 +2,168 @@ package exploring_mars.core;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.Assert;
 import org.junit.rules.ExpectedException;
 
 public class RocketNavigationTest {
 
 	RocketNavigation navigation;
-	
+
 	@Rule
 	public ExpectedException expectedEx = ExpectedException.none();
-	
+
 	@Test
 	public void rocketNavigation_setMaximumX_lessThanOne() throws ExploringMarsException {
-	
+
 		expectedEx.expect(ExploringMarsException.class);
 		expectedEx.expectMessage("x edge must be greater then 0.");
-		
+
 		navigation = new RocketNavigation(0, 10);
 	}
-	
+
 	@Test
 	public void rocketNavigation_setMaximumY_lessThanOne() throws ExploringMarsException {
-	
+
 		expectedEx.expect(ExploringMarsException.class);
 		expectedEx.expectMessage("y edge must be greater then 0.");
-		
+
 		navigation = new RocketNavigation(10, 0);
 	}
-	
+
+	@Test
+	public void rocketNavigation_validEdges() throws ExploringMarsException {
+		navigation = new RocketNavigation(10, 15);
+
+		Assert.assertEquals(10, navigation.getMaximumX());
+		Assert.assertEquals(15, navigation.getMaximumY());
+		Assert.assertEquals(new RocketCoordinate(0, 0, Direction.North), this.navigation.getCurrentCoordinate());
+	}
+
+	@Test
+	public void rocketNavigation_turnLeftOneTime() throws ExploringMarsException {
+		navigation = new RocketNavigation(10, 10);
+
+		for (int i = 0; i < 1; i++)
+			navigation.turnLeft();
+
+		Assert.assertEquals(new RocketCoordinate(0, 0, Direction.West), this.navigation.getCurrentCoordinate());
+	}
+
+	@Test
+	public void rocketNavigation_turnLeftTwoTimes() throws ExploringMarsException {
+		navigation = new RocketNavigation(10, 10);
+
+		for (int i = 0; i < 2; i++)
+			navigation.turnLeft();
+
+		Assert.assertEquals(new RocketCoordinate(0, 0, Direction.South), this.navigation.getCurrentCoordinate());
+	}
+
+	@Test
+	public void rocketNavigation_turnLeftThreeTimes() throws ExploringMarsException {
+		navigation = new RocketNavigation(10, 10);
+
+		for (int i = 0; i < 3; i++)
+			navigation.turnLeft();
+
+		Assert.assertEquals(new RocketCoordinate(0, 0, Direction.East), this.navigation.getCurrentCoordinate());
+	}
+
+	@Test
+	public void rocketNavigation_turnLeftFourTimes() throws ExploringMarsException {
+		navigation = new RocketNavigation(10, 10);
+
+		for (int i = 0; i < 4; i++)
+			navigation.turnLeft();
+
+		Assert.assertEquals(new RocketCoordinate(0, 0, Direction.North), this.navigation.getCurrentCoordinate());
+	}
+
+	@Test
+	public void rocketNavigation_turnRightOneTime() throws ExploringMarsException {
+		navigation = new RocketNavigation(10, 10);
+
+		for (int i = 0; i < 1; i++)
+			navigation.turnRight();
+
+		Assert.assertEquals(new RocketCoordinate(0, 0, Direction.East), this.navigation.getCurrentCoordinate());
+	}
+
+	@Test
+	public void rocketNavigation_turnRightTwoTimes() throws ExploringMarsException {
+		navigation = new RocketNavigation(10, 10);
+
+		for (int i = 0; i < 2; i++)
+			navigation.turnRight();
+
+		Assert.assertEquals(new RocketCoordinate(0, 0, Direction.South), this.navigation.getCurrentCoordinate());
+	}
+
+	@Test
+	public void rocketNavigation_turnRightThreeTimes() throws ExploringMarsException {
+		navigation = new RocketNavigation(10, 10);
+
+		for (int i = 0; i < 3; i++)
+			navigation.turnRight();
+
+		Assert.assertEquals(new RocketCoordinate(0, 0, Direction.West), this.navigation.getCurrentCoordinate());
+	}
+
+	@Test
+	public void rocketNavigation_turnRightFourTimes() throws ExploringMarsException {
+		navigation = new RocketNavigation(10, 10);
+
+		for (int i = 0; i < 4; i++)
+			navigation.turnRight();
+
+		Assert.assertEquals(new RocketCoordinate(0, 0, Direction.North), this.navigation.getCurrentCoordinate());
+	}
+
+	@Test
+	public void rocketNavigation_moveAboveMaximumX() throws ExploringMarsException {
+
+		expectedEx.expect(ExploringMarsException.class);
+		expectedEx.expectMessage("You've reached the boundaries of mars.");
+
+		navigation = new RocketNavigation(10, 10);
+		for (int i = 0; i < 11; i++)
+			navigation.move();
+	}
+
+	@Test
+	public void rocketNavigation_moveBelowZeroX() throws ExploringMarsException {
+
+		expectedEx.expect(ExploringMarsException.class);
+		expectedEx.expectMessage("You've reached the boundaries of mars.");
+
+		navigation = new RocketNavigation(10, 10);
+		navigation.turnLeft();
+		navigation.turnLeft();
+		navigation.move();
+	}
+
+	@Test
+	public void rocketNavigation_moveAboveMaximumY() throws ExploringMarsException {
+
+		expectedEx.expect(ExploringMarsException.class);
+		expectedEx.expectMessage("You've reached the boundaries of mars.");
+
+		navigation = new RocketNavigation(10, 10);
+		navigation.turnRight();
+
+		for (int i = 0; i < 11; i++)
+			navigation.move();
+	}
+
+	@Test
+	public void rocketNavigation_moveBelowZeroY() throws ExploringMarsException {
+
+		expectedEx.expect(ExploringMarsException.class);
+		expectedEx.expectMessage("You've reached the boundaries of mars.");
+
+		navigation = new RocketNavigation(10, 10);
+		navigation.turnLeft();
+		navigation.move();
+	}
+
 }
